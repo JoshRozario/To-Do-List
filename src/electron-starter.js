fa8b1d53dd15f49c28eadf40735016932865c142
JBobby <30451379+Jbobbie@users.noreply.github.com>
2019-06-23 20:25:06 +1000

    Added tray functionality

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,14 +1,10 @@
-const electron = require('electron');
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const {Menu,electron,Tray,app, BrowserWindow} = require('electron');
+
 
 const path = require('path');
 const url = require('url');
+const iconPath = path.join(__dirname+'/components/icon/favicon.ico')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 const startUrl = process.env.ELECTRON_START_URL || url.format({
@@ -18,41 +14,47 @@ const startUrl = process.env.ELECTRON_START_URL || url.format({
 });
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({transparent: true, frame: false, width:320 , height: 410, icon: __dirname+'/components/icon/favicon.ico'});
-    // and load the index.html of the app.
+    mainWindow = new BrowserWindow({transparent: true, frame: false, width:320 , height: 410, icon: iconPath, skipTaskbar: true});
     mainWindow.loadURL(startUrl);
+    tray = new Tray(iconPath);
+    var contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Show',
+            click: function(){
+                mainWindow.show()
+            }
+        },
+        {
+            label: 'Quit',
+            click: function(){
+                app.quit()
+            }
+        }
+    ])
 
-    // Emitted when the window is closed.
+    
+
+    tray.setToolTip("To-Do List");
+    tray.setContextMenu(contextMenu);
+    tray.on('double-click', function(){
+        mainWindow.show()
+    });
+
+
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
     })
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
